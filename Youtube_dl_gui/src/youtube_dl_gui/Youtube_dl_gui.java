60c07c5164f4f35513cf990b12cb546987949966
puru1joy <puru1joy@gmail.com>
2015-09-13 17:53:27 +0530

    added stuff

--- Youtube_dl_gui/src/youtube_dl_gui/Youtube_dl_gui.java
+++ Youtube_dl_gui/src/youtube_dl_gui/Youtube_dl_gui.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package youtube_dl_gui;
 
 import javafx.application.Application;
@@ -13,33 +12,57 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ListChangeListener.Change;
+import javafx.concurrent.Worker.State;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.web.PopupFeatures;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebHistory;
+import javafx.scene.web.WebHistory.Entry;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import netscape.javascript.JSObject;
 
 /**
  *
  * @author Puranjay
  */
 public class Youtube_dl_gui extends Application {
-    
+
+    private Scene scene;
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) {
+        // create scene
+        stage.setTitle("Youtube Downloader");
+        scene = new Scene(new Browser(), 750, 500, Color.web("#D32F2F"));
+        stage.setScene(scene);
+        // apply CSS style
+//        scene.getStylesheets().add("webviewsample/BrowserToolbar.css");
+        // show stage
+        stage.show();
     }
 
     /**
@@ -48,5 +71,47 @@ public class Youtube_dl_gui extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+}
+
+class Browser extends Region {
+
+    final WebView browser = new WebView();
+    final WebEngine webEngine = browser.getEngine();
+    final WebView smallView = new WebView();
+
+    public Browser() {
+        //apply the styles
+        getStyleClass().add("browser");
+
+        smallView.setPrefSize(120, 80);
+
+        webEngine.load(Youtube_dl_gui.class.getResource("index.html").toExternalForm());
+        webEngine.setUserStyleSheetLocation(getClass().getResource("roboto.css").toExternalForm());
+        getChildren().add(browser);
+    }
+
+    // JavaScript interface object
+    public class JavaApp {
+
+        public void exit() {
+            Platform.exit();
+        }
+    }
+
+    @Override
+    protected void layoutChildren() {
+        double w = getWidth();
+        double h = getHeight();
+        layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
+    }
+
+    @Override
+    protected double computePrefWidth(double height) {
+        return 750;
+    }
+
+    @Override
+    protected double computePrefHeight(double width) {
+        return 600;
+    }
 }
