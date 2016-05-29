6b995f0bf507c27ca0c39d98b65bfea78d062152
Puranjay Jain <puranjay.jain@st.niituniversity.in>
2016-05-29 10:12:29 +0530

    updated about page

--- src/views/About.js
+++ src/views/About.js
@@ -1,11 +1,36 @@
 import React from 'react'
 
+import {Card, CardHeader, CardText} from 'material-ui/Card'
+import {List, ListItem} from 'material-ui/List'
+import Avatar from 'material-ui/Avatar'
+
+const style = {
+  margin: '12px'
+}
+
 export default class About extends React.Component {
   render() {
+    const version = {
+      node: "Node Js Version " + window.process.versions.node,
+      chrome: "Chrome " + window.process.versions.chrome,
+      electron: "Electron " + window.process.versions.electron
+    }
+
     return (
-      <div>
-        <h2>About!</h2>
-      </div>
+       <Card style={style}>
+         <CardHeader
+           title="Youtube Dl GUI"
+           subtitle="v0.0.1"
+           avatar="http://lorempixel.com/100/100/nature/"
+         />
+         <CardText>
+           <List>
+             <ListItem primaryText={version.node} />
+             <ListItem primaryText={version.chrome} />
+             <ListItem primaryText={version.electron} />
+           </List>
+         </CardText>
+       </Card>
     )
   }
 }
