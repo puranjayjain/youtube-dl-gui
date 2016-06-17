70c9e6310b1b738bf73112d71aab1b82d83ca1e4
Puranjay Jain <puranjay.jain@st.niituniversity.in>
2016-06-17 20:50:46 +0530

    dropped obsolete deps fixed scroll to item in settings fixed settings not setting for desktop options

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,4 +1,5 @@
 import React, {PropTypes, Component} from 'react'
+import ReactDOM, {findDOMNode} from 'react-dom'
 
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
 import {List, ListItem} from 'material-ui/List'
@@ -12,9 +13,14 @@ import Toggle from 'material-ui/Toggle'
 // the settings loader helper
 import SettingsHandler from '../helpers/SettingsHandler'
 
-let stored = {}
-
-let settingsHandle = new SettingsHandler()
+let stored = {},
+settingsHandle = new SettingsHandler(),
+links = [
+  'Theme',
+  'Download Options',
+  'Filesystem Options',
+  'Desktop'
+]
 
 export default class Settings extends Component {
   // functions to do the settings update work
@@ -27,17 +33,21 @@ export default class Settings extends Component {
 
   }
 
-  // update the id setting
-  onIdToggle = () => {
-    stored.filesystem.id.toggleData()
+  // on touch tap go to link
+  onTouchLink = (index) => {
+    findDOMNode(this.refs['card'+ index]).scrollIntoView()
   }
+
+  // update the id setting
+  onIdToggle = () => stored.filesystem.id.toggleData()
 
   //  Restrict filenames to only ASCII characters, and avoid "&" and spaces in filenames
-  onRestrictToggle = () => {
-    stored.filesystem.restrict.toggleData()
-  }
+  onRestrictToggle = () => stored.filesystem.restrict.toggleData()
 
-  componentWillMount(){
+  // on toggle the desktop taskbar controller
+  onDesktopStatusToggle = () => stored.desktop.status.toggleData()
+
+  componentWillMount() {
     // load all the settings
     stored = settingsHandle.stored
   }
@@ -61,7 +71,8 @@ export default class Settings extends Component {
       main: {
         display: 'flex',
         flex: '8 0 auto',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        marginBottom: '1em'
       },
       innerText: {
         width: 'calc(100% - 2em)',
@@ -71,88 +82,104 @@ export default class Settings extends Component {
 
     return (
       <div style={style.container}>
-        <List style={style.toc}>
+        <List
+          style={style.toc}
+        >
           <Subheader>Jump To</Subheader>
-          <ListItem primaryText="Theme" />
-          <ListItem primaryText="Download Options" />
-          <ListItem primaryText="Filesystem Options" />
-          <ListItem primaryText="Desktop" />
+          {links.map( (row, index) => (
+            <ListItem
+              onTouchTap={() => this.onTouchLink(index)}
+              key={'li-' + index}
+              primaryText={row}
+            />
+          ))}
         </List>
         <div style={style.main}>
-          <Card style={style.card}>
-            <List>
-              <Subheader>Theme</Subheader>
-              <ListItem
-                primaryText="Use Dark Theme"
-                rightToggle= {
-                  <Toggle
-                    defaultToggled = {stored.theme.darkTheme.data}
-                    onToggle = {this.onDarkThemeToggle}
-                  />
-                }
-              />
-            </List>
-          </Card>
-          <Card style={style.card}>
-            <List>
-              <Subheader>Download Options</Subheader>
-              <TextField
-                style={style.innerText}
-                hintText="(default is 10),supports integer or infinite"
-                floatingLabelText="Maximum Number of retries"
-                value={stored.download.retries.data}
-              />
-              <TextField
-                style={style.innerText}
-                hintText="(default is 1024),supports integers"
-                floatingLabelText="Size of download buffer"
-                value={stored.download.bufferSize.data}
-              />
-            </List>
-          </Card>
-          <Card style={style.card}>
-            <List>
-              <Subheader>Filesystem Options</Subheader>
-              <ListItem
-                primaryText="Use only video ID in file name"
-                leftCheckbox= {
-                  <Checkbox
-                    defaultChecked = {stored.filesystem.id.data}
-                    onCheck = {this.onIdToggle}
-                  />
-                }
-              />
-              <TextField
-                style={style.innerText}
-                hintText="Use without destination folder"
-                floatingLabelText="Output filename template "
-                value={stored.filesystem.output.data}
-              />
-              <ListItem
-                primaryText="Restrict filenames to only ASCII characters"
-                leftCheckbox = {
-                  <Checkbox
-                    defaultChecked = {stored.filesystem.restrict.data}
-                    onCheck = {this.onIdToggle}
-                  />
-                }
-              />
-            </List>
-          </Card>
-          <Card style={style.card}>
-            <List>
-              <Subheader>Desktop</Subheader>
-              <ListItem
-                primaryText="Show download status in taskbar"
-                leftCheckbox= {
-                  <Checkbox
-                    defaultChecked = {stored.desktop.status.data}
-                    onCheck = {this.onIdToggle}
-                  />
-                }
-              />
-            </List>
-          </Card>
+          {links.map((row, index) => (
+            <Card
+              ref={'card' + index}
+              key={'item-' + index}
+              style={style.card}
+            >
+              <List>
+                <Subheader>{row}</Subheader>
+                {((index) => {
+                  switch (index) {
+                    case 1:
+                    return (
+                      <div >
+                        <TextField
+                          style={style.innerText}
+                          hintText="(default is 10),supports integer or infinite"
+                          floatingLabelText="Maximum Number of retries"
+                          value={stored.download.retries.data}
+                        />
+                        <TextField
+                          style={style.innerText}
+                          hintText="(default is 1024),supports integers"
+                          floatingLabelText="Size of download buffer"
+                          value={stored.download.bufferSize.data}
+                        />
+                      </div>
+                    )
+                    case 2:
+                    return (
+                    <div>
+                      <ListItem
+                        primaryText="Use only video ID in file name"
+                        leftCheckbox= {
+                          <Checkbox
+                            defaultChecked = {stored.filesystem.id.data}
+                            onCheck = {this.onIdToggle}
+                          />
+                        }
+                      />
+                      <TextField
+                        style={style.innerText}
+                        hintText="Use without destination folder"
+                        floatingLabelText="Output filename template "
+                        value={stored.filesystem.output.data}
+                      />
+                      <ListItem
+                        primaryText="Restrict filenames to only ASCII characters"
+                        leftCheckbox = {
+                          <Checkbox
+                            defaultChecked = {stored.filesystem.restrict.data}
+                            onCheck = {this.onIdToggle}
+                          />
+                        }
+                      />
+                    </div>
+                    )
+                    case 3:
+                    return (
+                    <ListItem
+                      primaryText="Show download status in taskbar"
+                      leftCheckbox= {
+                        <Checkbox
+                          defaultChecked = {stored.desktop.status.data}
+                          onCheck = {this.onDesktopStatusToggle}
+                        />
+                      }
+                    />
+                    )
+                    // case 0 is at the last
+                    default:
+                    return (
+                    <ListItem
+                      primaryText="Use Dark Theme"
+                      rightToggle= {
+                        <Toggle
+                          defaultToggled={stored.theme.darkTheme.data}
+                          onToggle={this.onDarkThemeToggle}
+                        />
+                      }
+                    />)
+                  }
+                })(index)}
+              </List>
+            </Card>
+          ))}
         </div>
       </div>
     )
