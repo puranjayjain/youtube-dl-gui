d9273ca16c5fa7734d6eb508de6333967ddb5862
Puranjay Jain <puranjay.jain@st.niituniversity.in>
2016-06-26 20:42:20 +0530

    added wget for updating ytdl versions

--- src/helpers/YtdlUpdater.js
+++ src/helpers/YtdlUpdater.js
@@ -3,33 +3,47 @@ import SettingsHandler from '../helpers/SettingsHandler'
 import moment from 'moment'
 
 let settingsHandle = new SettingsHandler(),
-stored = settingsHandle.stored
+stored = settingsHandle.stored,
+ytdlProcess
 
 export default class YtdlUpdater {
   constructor(published_at, url) {
-    // TODO if the new published_at is new stop all downloads and then update ytdl
-    // copy args
-    this.published_at = published_at
-    this.url = url
-    // set partial to true and updating to true so the downloading is being done is told
-    // update checking time
-    // let ytdl = stored.youtubedl.data
-    // TODO update tried and published_at after successful update to new version
-    // ytdl.published_at = this.published_at
-    // ytdl.lasttried = moment()
-
-    // update the stored data
-    // settingsHandle.setStored('youtubedl', ytdl)
+    // start updating
+    startUpdateExe(published_at, url)
   }
 
-  startUpdateExe = () => {
+  startUpdateExe = (published_at, url) => {
     which('youtube-dl', (err, resolvedPath) => {
       // err is returned if no "exe" is found on the PATH
       // if it is found, then the absolute path to the exec is returned
-      if (err) throw err
+      if (err) {
+        throw err
+        return
+      }
 
-      let pathToExe = resolvedPath.slice(0, resolvedPath.lastIndexOf('\\') - resolvedPath.length + 1)
-      
-    })
-  }
+      let pathToExe = resolvedPath.slice(0, resolvedPath.lastIndexOf('\\') - resolvedPath.length + 1),
+      fileName = 'youtube-dl.exe.mtd',
+      filePath = path.join(pathToExe, fileName)
+
+      // start download of youtube-dl
+      wget({
+        url:  url,
+        dest: filePath,      // destination path or path with filenname, default is ./
+        timeout: 10000       // duration to wait for request fulfillment in milliseconds, default is 2 seconds
+      }, (error, response, body) => {
+        if (error) {
+          console.log('--- error:')
+          console.log(error)            // error encountered
+        } else {
+          // update the stored data
+          settingsHandle.updateStores('youtubedl', {
+            published_at: published_at,
+            lasttried: moment()
+          })
+          // TODO if the new published_at is new stop all downloads and then update ytdl
+        }
+      }
+    )
+  })
+}
 }
